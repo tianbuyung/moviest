433d302c5bc567edfd21d5eb8ba19b326be2f7f7
Septian Maulana <septianm028@gmail.com>
2022-11-12 18:43:00 +0700

    [tianbuyung] - created component structure & routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => <div>App</div>;
+import { Movies, Profile, MovieInformation, Actors, Navbar } from 'components';
+
+const App = () => (
+  <div>
+    <CssBaseline />
+    <Navbar />
+    <main>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/movie/:id" element={<MovieInformation />} />
+        <Route path="/actors/:id" element={<Actors />} />
+      </Routes>
+    </main>
+  </div>
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Movies } from 'components/Movies/Movies';
+export { default as Profile } from 'components/Profile/Profile';
+export { default as MovieInformation } from 'components/MovieInformation/MovieInformation';
+export { default as Actors } from 'components/Actors/Actors';
+export { default as Navbar } from 'components/NavBar/NavBar';
