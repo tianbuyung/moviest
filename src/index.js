ec6517eeddf7a5a28142bd7c2d82732fb236390c
Septian Maulana <septianm028@gmail.com>
2022-11-13 21:36:14 +0700

    [tianbuyung] - created redux setup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from 'components/App';
+import store from 'app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const theme = createTheme({});
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
