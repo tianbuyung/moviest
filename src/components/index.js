68f93466444d0ce13cfacd737f28ef28d324fbe1
Septian Maulana <septianm028@gmail.com>
2022-11-13 15:23:57 +0700

    [tianbuyung] - created navbar and sidebar component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Profile } from 'components/Profile/Profile';
 export { default as MovieInformation } from 'components/MovieInformation/MovieInformation';
 export { default as Actors } from 'components/Actors/Actors';
 export { default as Navbar } from 'components/NavBar/NavBar';
+export { default as Sidebar } from 'components/Sidebar/Sidebar';
--- src/components/styles.js
+++
@@ -1,8 +0,0 @@
-import { Box, styled } from '@mui/material';
-
-const rootStyles = styled(Box)({
-  display: 'flex',
-  height: '100%',
-});
-
-export default rootStyles;
